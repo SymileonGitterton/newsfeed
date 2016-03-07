665560211699ed89595643b462e8db0af157bcf2
Peter Arnold <peter_arnold@SlaterMax.local>
2016-03-07 14:26:56 -0800

    client-side seasrch working

--- public/js/search-model.js
+++ public/js/search-model.js
@@ -12,7 +12,34 @@
    *  results -- an array of search results
    */
   SearchModel.search = function(query, callback) {
-    // TODO
+    console.log("incoming query: "+query);
+    query = encodeURIComponent(query);
+    console.log("encoded query: "+query);
+
+    // 1) create an XMLHttpRequest object
+    var hackReq = new XMLHttpRequest();
+    
+    // 2) Handle the 'load' event with this code:
+    hackReq.addEventListener('load', function() {                 // callback for when load completes from server
+            if (hackReq.status != STATUS_OK) {
+              callback(hackReq.responseText);
+            } else {
+              callback(null,JSON.parse(hackReq.responseText));
+            }
+        });
+
+    // 3) open a URL with the correct request type
+    //hackURL = "http://127.0.0.1:3000"+SEARCH_URL+"/?query="+query;
+    hackURL = SEARCH_URL+"/?query="+query;
+    console.log("encoded search URL: "+hackURL);
+    hackReq.open('GET', hackURL);
+
+    // 4) set the content-type header, if this is a POST
+    hackReq.setRequestHeader("Content-type", "application/json");
+
+    // 5) send the request to the server, with parameters if a POST
+    hackReq.send(); 
+    // done; exit and await 'load' callback
   };
 
   window.SearchModel = SearchModel;
--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,33 @@ var searchesReturned = 0;
 
 var SEARCH_TARGET_COUNT = 3;
 
-var collectResultsAndPublishIfReady = function(error, results) {
+var collectResultsAndPublishIfReady = function(error, results, source, response) {
 	// log what came back
+	console.log("I am the callback that was passed to "+source+" search()");
+  	console.log("[publishIfReady] response has type: "+typeof(response)); 
+
 	if (error != null) {
 		console.log("ERROR: "+error);	// first parameter not null? an error happened
 	} else {
-		for(var i=0;i<results.length;i++) {
-			console.log(i+" "+results[i].id+" "+results[i].title);
-		}
+		console.log(results.length+" results received");
+		//for(var i=0;i<results.length;i++) {
+		//	console.log(i+" "+results[i].id+" "+results[i].title);
+		//}
 	}
 	// prepare a result if we got one
-	if (results.length >0) {
-		results[0].api = 'soundcloud';
+	if (results.length > 0) {
+		results[0].api = source;
 		resultsSet.push(results[0]);
 	}
 	// render and return html, if all done...
 	if (++searchesReturned >= SEARCH_TARGET_COUNT) {		// naked increment of global is not thread safe!!!!
 		console.log("got the whole result set");
 		for(var i=0;i<resultsSet.length;i++) {
-			console.log(resultsSet[i].api+" "+resultsSet[i].id+" "+resultsSet[i].title);
+			console.log(resultsSet[i].api+" title="+resultsSet[i].title+"  source="+resultsSet[i].source);
 		}
-		// now somehow render the resultsSet and give back to the client?
+		// now somehow render the resultsSet and give back to the client? as JSON?
+		//response.setHeader('Content-Type', 'application/json');
+		response.send(JSON.stringify(resultsSet));
 	}
 };
 
@@ -39,35 +45,27 @@ module.exports = function(app) {
   	});
 
   	app.get('/search', function(request, response) {
+		searchesReturned = 0;
 		resultsSet = [];
-  		var qstring = request.query.query.replace(' ', '%20');
-  		//console.log("request.query.query: "+request.query.query+" type "+typeof(request.query.query)+" qstring: "+qstring);
+  		var qstring = request.query.query.replace(/ /g, '%20');
+  		console.log("request.query.query: "+request.query.query+" type "+typeof(request.query.query)+" qstring: "+qstring);
+  		console.log("this is happening because a search came in");
 
   		// 1. search soundclound
-  		api_soundcloud.search(qstring, function(error, results) {
-			console.log("I am the callback that was passed to soundcloud search()\n");
-			//if (error != null) {
-			//	console.log("ERROR: "+error);	// first parameter not null? an error happened
-			//} else {
-			//	for(var i=0;i<results.length;i++) {
-			//		console.log(i+" "+results[i].id+" "+results[i].title);
-			//	}
-			//}
-			collectResultsAndPublishIfReady(error,results);
+  		api_soundcloud.search(qstring, function(error,results) {
+			collectResultsAndPublishIfReady(error,results,'soundcloud',response)
   		}); // end of youtube search
 
 
   		// 2. search YubeToob
-  		api_youtube.search(qstring, function(error, results) {
-			console.log("I am the callback that was passed to youtube search()\n");
-			collectResultsAndPublishIfReady(error,results);
-  		}); // end of soundcloud search
+  		api_youtube.search(qstring, function(error,results) {
+			collectResultsAndPublishIfReady(error,results,'youtube',response)
+		}); // end of soundcloud search
 
 
   		// 3. search fuckr
-  		api_flickr.search(qstring, function(error, results) {
-			console.log("I am the callback that was passed to flickr search()\n");
-			collectResultsAndPublishIfReady(error,results);
+  		api_flickr.search(qstring, function(error,results) {
+  			collectResultsAndPublishIfReady(error,results,'flickr',response)
   		}); // end of flickr search
 
   	});
